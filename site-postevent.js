6b3cfcc75d09f0a598dcb5fa83999285faa6364d
NOTO-ReBloom <ryoya20041219@gmail.com>
2026-09-24 20:58:47 +0900

    Recognize optimized page CSS bundles

--- site-postevent.js
+++ site-postevent.js
@@ -2,7 +2,7 @@
   'use strict';
   window.__RB_POSTEVENT_RUNTIME__=true;
   const ensureRuntimeStyle=()=>{
-    if(document.querySelector('link[href*="site-runtime-20260924.css"]')) return;
+    if(document.querySelector('link[href*="site-runtime-20260924.css"],link[href*="-optimized-20260924.css"]')) return;
     const link=document.createElement('link');
     link.rel='stylesheet';
     link.href='site-runtime-20260924.css?v=1';
