500d0c496c67bd08d99e58ed3f8e1992cba70228
NOTO-ReBloom <ryoya20041219@gmail.com>
2026-09-24 21:21:13 +0900

    Remove duplicate dynamic site-core execution

--- site-postevent.js
+++ site-postevent.js
@@ -164,8 +164,4 @@
     }
   });
 
-  const coreScript=document.createElement('script');
-  coreScript.src='site-core.js?v=20260924perf2';
-  coreScript.async=false;
-  document.body.appendChild(coreScript);
 })();
