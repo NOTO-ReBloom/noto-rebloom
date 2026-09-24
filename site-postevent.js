8fbb98253f009b6b8bfe11a24f8b2fd267ae3d0f
NOTO-ReBloom <ryoya20041219@gmail.com>
2026-09-24 12:38:02 +0900

    Bump post-event core cache after collision fix

--- site-postevent.js
+++ site-postevent.js
@@ -70,5 +70,5 @@
     }
   });
 
-  document.write('<script src="site-core.js?v=20260924postevent1"><\/script>');
+  document.write('<script src="site-core.js?v=20260924postevent2"><\/script>');
 })();
